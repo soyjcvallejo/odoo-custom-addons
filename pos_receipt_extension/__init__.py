e0196bab6aa0116ca9c9fca95e306e001cc98bfc
Juan Carlos Vallejo <jc.vallejo@opensupport.com.bo>
2026-02-14 14:14:28 -0400

    [ADD] pos_receipt_extension: initial structure for Odoo 18.

---
+++ pos_receipt_extension/__init__.py
@@ -0,0 +1,3 @@
+# Copyright 2026 Open Support
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0)
+
---
+++ pos_receipt_extension/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright 2026 Open Support
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0)
+
+{
+    "name": "POS Receipt Extension",
+    "summary": "Extends the default POS receipt layout",
+    "version": "18.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Sales/Point of Sale",
+    "website": "https://github.com/soyjcvallejo/odoo-custom-addons",
+    "author": "Open Support",
+    "maintainers": ["soyjcvallejo"],
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "auto_install": False,
+    "depends": [
+        "point_of_sale",
+    ],
+    "assets": {
+        "point_of_sale._assets_pos": [
+            "pos_receipt_extension/static/src/xml/pos_receipt_extension.xml",
+        ],
+    },
+}
